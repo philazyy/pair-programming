721f435e2e5cd13fd4ae3769cd734138a87d71d5
PHIL HÄGELI <phil.haegeli8@gmail.com>
2025-05-08 08:09:51 +0200

    Code für Payslip

--- src/payroll.ts
+++ src/payroll.ts
@@ -3,9 +3,9 @@ export type Salary = {
   payday: Date;
   gross: number;
 };
-
+ 
 export type Deductions = Map<string, number>;
-
+ 
 export const DEDUCTION_RATES: Deductions = new Map([
   ["AHV", 8.7],
   ["IV", 1.4],
@@ -14,16 +14,53 @@ export const DEDUCTION_RATES: Deductions = new Map([
   ["NBU", 0.73],
   ["PK", 8.9],
 ]);
-
+ 
 export type Payslip = {
   salary: Salary;
   deductions: Deductions;
   totalDeductions: number;
   net: number;
 };
-
+ 
 export function calculatePayslip(salary: Salary): Payslip {
   // TODO: implement
+  const birthdate = new Date(salary.born);
+  const payday = new Date(salary.payday);
+  const age = payday.getFullYear() - birthdate.getFullYear();
+  const turened17 = new Date(birthdate.getFullYear() + 17, 0, 1);
+  const isOlderThan17 = payday >= turened17;
+  const yearlyGross = salary.gross * 12;
+  const deductions: Deductions = new Map();
+ 
+  if (isOlderThan17)
+  {
+    for (const name of ["AHV", "IV", "EO"]) {
+      const rate = DEDUCTION_RATES.get(name);
+      deductions.set(name, +(salary.gross * (rate / 100)).toFixed(2));
+    }
+  }
+ 
+  if (yearlyGross >= 2500)
+  {
+    for (const name of ["ALV", "NBU"]) {
+      const rate = DEDUCTION_RATES.get(name);
+      deductions.set(name, +(salary.gross * (rate / 100)).toFixed(2));
+    }
+  }
+ 
+  if (yearlyGross >= 22680)
+  {
+    const rate = DEDUCTION_RATES.get("PK");
+    deductions.set("PK", +(salary.gross * (rate / 100)).toFixed(2));
+  }
+ 
+  let totalDeductions = 0;
+  for (const value of deductions.values()){
+    totalDeductions += value;
+  }
+ 
+  const net = (salary.gross - totalDeductions).toFixed(2);
+ 
   const result: Payslip = {
     salary: salary,
     deductions: new Map(),
@@ -32,3 +69,4 @@ export function calculatePayslip(salary: Salary): Payslip {
   };
   return result;
 }
+ 
